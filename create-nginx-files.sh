e5da2267f629265f61baa2e5a6bed8f397bce4a8
Ricard <ricardpons@me.com>
2020-05-31 01:13:19 +0900

    Create nginx file

---
+++ create-nginx-files.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Nginx - new server block
+
+# Functions
+ok() { echo -e '\e[32m'$1'\e[m'; } # Green
+die() { echo -e '\e[1;31m'$1'\e[m'; exit 1; }
+
+# Variables
+NGINX_AVAILABLE_VHOSTS='/etc/nginx/sites-available'
+NGINX_ENABLED_VHOSTS='/etc/nginx/sites-enabled'
+
+# Sanity check
+[ $(id -g) != "0" ] && die "Script must be run as root."
+[ $# != "1" ] && die "Usage: $(basename $0) domainName"
+
+# Create nginx config file
+cat > $NGINX_AVAILABLE_VHOSTS/$1 <<EOF
+
+upstream php {
+    server unix:/tmp/php-cgi.socket;
+    server 127.0.0.1:9000;
+}
+
+server {
+    server_name $1;
+    root /var/www/$1;
+
+    index index.php;
+
+    location = /favicon.ico {
+        log_not_found off;
+        access_log off;
+    }
+
+    location = /robots.txt {
+        allow all;
+        log_not_found off;
+        access_log off;
+    }
+
+    location / {
+        try_files $uri $uri/ /index.php?$args;
+    }
+
+    location ~ \.php$ {
+        #NOTE: You should have "cgi.fix_pathinfo = 0;" in php.ini
+        include fastcgi_params;
+        fastcgi_intercept_errors on;
+        fastcgi_pass php;
+    }
+
+    location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
+        expires max;
+        log_not_found off;
+    }
+}
+
+EOF
+
+# Enable site by creating symbolic link
+ln -s $NGINX_AVAILABLE_VHOSTS/$1 $NGINX_ENABLED_VHOSTS/$1
+
+# Restart
+/etc/init.d/nginx reload
+
+ok "Site Created for $1"
